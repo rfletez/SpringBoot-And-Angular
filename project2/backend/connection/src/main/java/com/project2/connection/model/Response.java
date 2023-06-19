a358534e6ab5cf1e46b0a964dd5dcebdb3fdbebf
Rosny Fletez Jr <rfletezjr@gmail.com>
2023-06-19 09:05:58 -0700

    Created a Response class for user request/response

--- project2/backend/connection/src/main/java/com/project2/connection/model/Response.java
+++ project2/backend/connection/src/main/java/com/project2/connection/model/Response.java
@@ -1,5 +1,27 @@
 package com.project2.connection.model;
 
-public class Response {
+import java.time.LocalDateTime;
+import java.util.Map;
 
+import org.springframework.http.HttpStatus;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+import lombok.Data;
+import lombok.experimental.SuperBuilder;
+
+
+@Data
+@SuperBuilder
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class Response {
+	protected LocalDateTime timeStamp;
+	protected int statusCode;
+	protected HttpStatus status;
+	protected String reason;
+	protected String message;
+	protected String developerMessage;
+	protected Map<?, ?> data;
+	
+	
 }
